1d1f100b1d22b05b95e7bbd6a86c56ae0edaf2e1
Aghlimi <ahmed4aghlimi@gmail.com>
2026-01-08 18:39:53 +0100

    clean repo

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,11 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\AssignProvider::class,
     App\Providers\FeatureProvider::class,
+    App\Providers\MemberProvider::class,
     App\Providers\ProjectProvider::class,
+    App\Providers\TaskProvider::class,
     App\Providers\TelescopeServiceProvider::class,
     App\Providers\UserProvider::class,
 ];
